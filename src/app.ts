a568b17d36eb7a942881ad72c32800e6541c492a
rebesbc <19170549@itculiacan.edu.mx>
2023-11-11 21:32:42 -0700

    feat: Estados de respuesta HTTP

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import cors from "cors";
-import express, { Application } from "express";
 import ip from "ip";
+import express, { Application, response } from "express";
+import { HttpResponse } from "./domain/response";
+import { Code } from "./enum/code.enum";
+import { Status } from "./enum/status.enum";
 
 export class App {
     private readonly app: Application;
@@ -24,8 +27,8 @@ export class App {
     }
 
     private routes(): void {
-        this.app.use('/libros', (req, res) => {});
-        this.app.get('/', (req, res) => res.status(200).send({ message: 'Server is up' }));
-        this.app.all('*', (req, res) => res.status(404).send({ message: this.ROUTE_NOT_FOUND })); // todas las URL que no se encuentren
+        this.app.use('/books', (req, res) => {});
+        this.app.get('/', (_, res) => res.status(Code.OK).send(new HttpResponse(Code.OK, Status.OK, 'Welcome to the RestAPI for Books v1.0')));
+        this.app.all('*', (_, res) => res.status(Code.NOT_FOUND).send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, this.ROUTE_NOT_FOUND))); // todas las URL que no se encuentren
     }
 }
