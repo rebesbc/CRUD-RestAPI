259fef027923a87751d3b195876efcbadc1272f9
rebesbc <19170549@itculiacan.edu.mx>
2023-11-11 23:30:13 -0700

    feat: Setup de rutas

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,6 +1,15 @@
 import { Router } from 'express';
+import { createBook, deleteBook, getBook, getBooks, updateBook } from '../controller/book.controller';
 
 const bookRoutes = Router();
 
+bookRoutes.route('/') // [template: /patients]
+    .get(getBooks)
+    .post(createBook);
+
+bookRoutes.route('/:bookId') // [template: /patients/{bookId}] [example: /patients/1]
+    .get(getBook)
+    .put(updateBook)
+    .delete(deleteBook)
 
 export default bookRoutes;
