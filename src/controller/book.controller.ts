19484cbb69ed2005a1119041f30d5fd6c64a1f5c
rebesbc <19170549@itculiacan.edu.mx>
2023-11-11 23:13:13 -0700

    feat: [POST] Crear un nuevo elemento 'book'

--- src/controller/book.controller.ts
+++ src/controller/book.controller.ts
@@ -23,7 +23,7 @@ export const getBooks = async (req: Request, res: Response): Promise<Response<Bo
     }
 };
 
-export const getBook = async (req: Request, res: Response): Promise<Response<Book[]>> => {
+export const getBook = async (req: Request, res: Response): Promise<Response<Book>> => {
     console.info(`[${new Date().toLocaleString()}] Incoming ${req.method}${req.originalUrl} Request from ${req.rawHeaders[0]}${req.rawHeaders[1]}`) // host: req.rawHeaders[0], ip's host: req.rawHeaders[1]
     try {
         const pool = await connection();
@@ -37,6 +37,22 @@ export const getBook = async (req: Request, res: Response): Promise<Response<Boo
                 .send(new HttpResponse(Code.NOT_FOUND, Status.NOT_FOUND, 'Book not found'))
         }
         
+    } catch(error: unknown) {
+        console.error(error);
+        return res.status(Code.INTERNAL_SERVER_ERROR)
+            .send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, 'An error occurred'))
+    }
+};
+
+export const createBook = async (req: Request, res: Response): Promise<Response<Book>> => {
+    console.info(`[${new Date().toLocaleString()}] Incoming ${req.method}${req.originalUrl} Request from ${req.rawHeaders[0]}${req.rawHeaders[1]}`) // host: req.rawHeaders[0], ip's host: req.rawHeaders[1]
+    let book: Book = { ...req.body };
+    try {
+        const pool = await connection();
+        const result: ResultSet = await pool.query(QUERY.CREATE_BOOK, Object.values(book));
+        book = { id: (result[0] as ResultSetHeader).insertId, ...req.body }; // obtener información del book a través del id creado en su result
+        return res.status(Code.CREATED)
+                .send(new HttpResponse(Code.CREATED, Status.CREATED, 'Book created', book))
     } catch(error: unknown) {
         console.error(error);
         return res.status(Code.INTERNAL_SERVER_ERROR)
